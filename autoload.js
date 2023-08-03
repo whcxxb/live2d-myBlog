a4c2fd109a9269d0d1719e1b640e97073caebcc2
wangxiaobai <731896502@qq.com>
2023-08-03 22:40:18 +0800

    up

--- autoload.js
+++ autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
     // 配置选项的具体用法见 README.md
     initWidget({
       waifuPath: live2d_path + 'waifu-tips.json',
-      //apiPath: "https://live2d.fghrsh.net/api/",
-      apiPath: 'https://api.zsq.im/live2d/',
+      apiPath: "https://live2d.fghrsh.net/api/",
+      // apiPath: 'https://api.zsq.im/live2d/',
       // cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
       // tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
       tools: ['hitokoto', 'asteroids', 'switch-texture', 'info', 'quit']
